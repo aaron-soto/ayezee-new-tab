665e93de3354df873b40eba86608ce96038ad82b
aaron-soto <aaron.m.soto1@gmail.com>
2025-08-14 09:36:10 -0700

    feat: add missing Pinterest SVG logo to links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,37 +15,35 @@ export default async function Home() {
         <TimeDisplay />
       </div>
 
-      <div className="flex flex-wrap items-center justify-around gap-10 gap-y-12 md:justify-start">
+      <div className="grid grid-cols-4 md:grid-cols-12 gap-y-10 md:gap-y-12 md:justify-start">
         {links.map((link) => (
-          <a
-            key={link.label}
-            href={link.href}
-            className="relative flex items-center group drop-shadow-2xl"
-          >
-            <svg
-              viewBox="0 0 160 160"
-              xmlns="http://www.w3.org/2000/svg"
-              version="1.1"
-              fill="currentColor"
-              className="transition-all duration-100 ease-in-out cursor-pointer size-16 min-h-16 min-w-16 text-neutral-800 group-hover:text-neutral-700"
-            >
-              <path
-                d="M 0 80 C 0 0, 0 0, 80 0 S 160 0, 160 80, 160 160
-80 160, 0 160, 0 80"
-                transform="rotate(0, 80, 80) translate(0, 0)"
-              ></path>
-            </svg>
-            <Image
-              src={link.icon}
-              alt={link.label}
-              width={32}
-              height={32}
-              className="absolute -translate-x-1/2 size-8 left-1/2"
-            />
-            <span className="absolute text-sm font-medium -translate-x-1/2 -bottom-6 left-1/2 text-nowrap text-ellipsis">
-              {link.label}
-            </span>
-          </a>
+          <div key={link.label} className="flex flex-col items-center">
+            <a href={link.href} className="relative flex items-center group drop-shadow-2xl">
+              <svg
+                viewBox="0 0 160 160"
+                xmlns="http://www.w3.org/2000/svg"
+                version="1.1"
+                fill="currentColor"
+                className="transition-all duration-100 ease-in-out cursor-pointer size-16 min-h-16 min-w-16 text-neutral-800 group-hover:text-neutral-700 group-hover:scale-105"
+              >
+                <path
+                  d="M 0 80 C 0 0, 0 0, 80 0 S 160 0, 160 80, 160 160
+                80 160, 0 160, 0 80"
+                  transform="rotate(0, 80, 80) translate(0, 0)"
+                ></path>
+              </svg>
+              <Image
+                src={link.icon}
+                alt={link.label}
+                width={32}
+                height={32}
+                className="absolute -translate-x-1/2 group-hover:scale-105 size-8 left-1/2"
+              />
+              <span className="absolute text-sm font-medium -translate-x-1/2 -bottom-6 left-1/2 text-nowrap text-ellipsis">
+                {link.label}
+              </span>
+            </a>
+          </div>
         ))}
       </div>
     </div>
